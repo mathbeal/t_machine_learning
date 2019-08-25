c71a69f87e0a4e51af660197c798fdbd47aae881
Mathieu Beal <mathieu.beal@gmail.com>
2019-08-25 15:43:11 +0200

    first prediction with tree

--- 01_introduction/01_basic_flask.py
+++ 01_introduction/01_basic_flask.py
@@ -1,6 +1,11 @@
 from flask import Flask
-
+from flask import render_template
 # prediction means inference.
+import os
+import numpy as np
+
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 
 # keras is the deep learning library
 from keras.models import load_model
@@ -12,16 +17,25 @@ app = Flask(__name__)
 
 MODEL_PATH = 'models/my_model.h5'
 model = ResNet50(weights='imagenet')
+model._make_predict_function()
 
-def model_predict(img_path, model):
-    img = image.load_image(img_path)
+def model_predict(img_path='./tree.jpg', model=model):
+    from datetime import datetime
+    start = datetime.now()
+    assert os.path.isfile(img_path), f'missing file {img_path}'
+    img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
-    preds = models.predict(x)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    print(decode_predictions(preds, top=10))
+    print("DURATION:", (datetime.now()-start).total_seconds())
     return preds
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print("model prediction", model_predict())
+    return render_template('./index.html')
 
 @app.route('/predict')
 def upload():
@@ -29,10 +43,8 @@ def upload():
     """
     if request.method == 'POST':
         f = request.files['file']
-
         f.save(file_path)
         preds = model_predict(file_path, model)
-
         pred_class = decode_predictions(preds, top=1)
         return pred_class
 
